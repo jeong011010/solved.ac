5c4e9da113af3e1045290cb75de50339900ba0e3
jeong011010 <jeong011010@naver.com>
2022-01-14 23:30:09 +0900

    ADD folder [1967]

---
+++ solved.ac/1967/1967.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int n, len;
+
+
+void input(){
+
+}
+
+void f(){
+
+}
+
+void solve(){
+
+}
+
+int main(){
+    solve();
+    return 0;
+}
