c0e418c004054b78820af3a7cba2baf5ffd5db11
jeong011010 <jeong011010@naver.com>
2022-02-07 23:03:18 +0900

    add cpp file [13172.cpp]

---
+++ solved.ac/13172/13172.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int M;
+
+void input(){
+    cin>>M;
+    int N,S;
+    for(int i=0;i<M;i++){
+        cin>>N>>S;
+        
+    }
+}
+
+int main(){
+    return 0;
+}
