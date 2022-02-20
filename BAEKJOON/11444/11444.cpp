323ea56d9b02b298bc5b8b67d27acef43a92f1c2
Kim._.jazz <jeong011010@naver.com>
2022-02-20 18:59:07 +0900

    update cpp file [11444.cpp]

--- BAEKJOON/11444/11444.cpp
+++ BAEKJOON/11444/11444.cpp
@@ -5,11 +5,11 @@ int arr[10];
 long long ans;
 long long n;
 
-long long Fibo(int n){
+/*long long Fibo(int n){
     if(n==0) return 0;
     if(n==1) return 1;
     
-}
+}*/
 
 void solve(){
     cin>>n;
