221f744f11587eb1d9d54f372198765bc517ee76
Kim._.jazz <jeong011010@naver.com>
2022-02-22 23:52:12 +0900

    add cpp file [codeforce-A.cpp]

--- BAEKJOON/14003/14003.cpp
+++ BAEKJOON/14003/14003.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int N;
 vector<long long> A, idx, ans;
 
-
-void f(){
-    int cnt=0;
-    for(int i=0;i<N;i++){
-        cin>>A[i];
-        
-        auto pos = lower_bound(ans[1], ans[cnt+1], A[i]);
-        *pos = A[i];
-        idx[i] = distance(A, pos);
+void f()
+{
+    int cnt = 0;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
     }
 }
 
-void solve(){
-    cin>>N;
-    
+void solve()
+{
+    cin >> N;
 }
 
-int main(){
+int main()
+{
     return 0;
 }
---
+++ codeforces/Educational Codeforces Round 123/A-Doors and Keys/A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int t;
+
+bool check(string way)
+{
+    bool r, g, b;
+    r = g = b = false;
+    for (int i = 0; i < 6; i++)
+    {
+        if (way[i] == 'r')
+            r = true;
+        else if (way[i] == 'g')
+            g = true;
+        else if (way[i] == 'b')
+            b = true;
+        else if (way[i] == 'R' && !r)
+            return false;
+        else if (way[i] == 'G' && !g)
+            return false;
+        else if (way[i] == 'B' && !b)
+            return false;
+    }
+    return true;
+}
+
+void solve()
+{
+
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        string way;
+
+        cout << (check(way) ? "YES" : "NO") << '\n';
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
