1e4f9b1bb4f9d9d6062e2e859a13f12cc18243aa
Kim._.jazz <jeong011010@naver.com>
2022-02-24 02:04:59 +0900

    update cpp file [10830.cpp]

--- BAEKJOON/10830/10830.cpp
+++ BAEKJOON/10830/10830.cpp
@@ -1,44 +1,70 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-vector<int> Arr[5], tmp[5];
-int N,B;
-int A[5][5];
+int Arr[5][5];
+int N;
+long long B;
 
-void input(){
-    cin>>N>>B;
-    for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++)
-            cin>>A[i][j];
-}
-
-void mul(vector<int> X[5], vector<int> Y[5]){
-    vector<int> tmp[5];
-    for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++){
-            tmp[i][j]=0;
-            for(int k=0;k<N;k++)
-                tmp[i][j] += (X[i][k]*Y[k][j]);
-            tmp[i][j] %= 1000;
+void mul(int F[5][5], int M[5][5])
+{
+    int tmp[5][5] = {
+        0,
+    };
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                tmp[i][j] += (F[i][k] * M[k][j]);
+            }
         }
-}
-
-vector<int> fpow(vector<int> C, int n){
-    if(n==1) return C;
-    vector<int> x = fpow(C,n/2);
-    if(n%2==0) return;
-    else return;
-}
-
-void solve(){
-    input();
-    for(int i=0;i<B;i++){
-
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            F[i][j] = tmp[i][j] % 1000;
+        }
     }
 }
 
-int main(){
+void power(int F[5][5], long long n)
+{
+    if (n == 0 || n == 1)
+        return;
+    power(F, n / 2);
+    mul(F, F);
+    if (n % 2 != 0)
+        mul(F, Arr);
+}
+
+void print(int Ans[5][5])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            cout << Ans[i][j] % 1000 << ' ';
+        cout << '\n';
+    }
+}
+
+void solve()
+{
+    cin >> N >> B;
+    int tmp[5][5];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            cin >> Arr[i][j];
+            tmp[i][j] = Arr[i][j];
+        }
+    power(tmp, B);
+    print(tmp);
+}
+
+int main()
+{
     solve();
     return 0;
 }
