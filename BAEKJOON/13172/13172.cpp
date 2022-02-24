db93596667b9a12b0a8dcfeff8eb5a31d237f548
Kim._.jazz <jeong011010@naver.com>
2022-02-24 16:02:18 +0900

    update cpp file [13172.cpp]

--- BAEKJOON/13172/13172.cpp
+++ BAEKJOON/13172/13172.cpp
@@ -1,17 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 
-int M;
-
-void input(){
-    cin>>M;
-    int N,S;
-    for(int i=0;i<M;i++){
-        cin>>N>>S;
-        
+long long power(long long n, long long m)
+{
+    long long ret = 1;
+    while (m)
+    {
+        if (m & 1)
+            ret = ret * n % MOD;
+        m = m / 2;
+        n = n * n % MOD;
     }
+    return ret;
 }
 
-int main(){
+void solve()
+{
+    long long ans = 0, N, S;
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> N >> S;
+        ans += (S * (power(N, MOD - 2))) % MOD;
+    }
+    cout << ans % MOD;
+}
+
+int main()
+{
+    solve();
     return 0;
 }
