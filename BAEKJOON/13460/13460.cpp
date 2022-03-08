2bdb891e844b483fda0b15a55eb5720b625ee9ac
Kim._.jazz <jeong011010@naver.com>
2022-03-08 14:06:03 +0900

    update cpp file [13460.cpp]

--- BAEKJOON/13460/13460.cpp
+++ BAEKJOON/13460/13460.cpp
@@ -4,10 +4,51 @@
 using namespace std;
 
 string arr[10];
+int Rx, Ry, Bx, By, Ox, Oy;
 int N, M;
+int My[4] = {0, -1, 0, 1};
+int Mx[4] = {1, 0, -1, 0};
+
+typedef struct ball
+{
+    int ry, rx, by, bx, m = 0;
+} ball;
 
 void bfs()
 {
+    queue<ball> B;
+    B.push({Ry, Rx, By, Bx});
+    while (!B.empty())
+    {
+        ball b = B.front();
+        B.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int a = b.ry + My[i];
+            int b = b.ry + My[i];
+            while (arr[b.ry + My[i]][b.rx + Mx[i]] == '.' || arr[b.by + My[i]][b.bx + Mx[i]] == '.')
+            {
+                if (arr[b.ry + My[i]][b.rx + Mx[i]] == '.')
+                {
+                    b.ry += My[i];
+                    b.rx += Mx[i];
+                }
+                if (arr[b.by + My[i]][b.bx + Mx[i]] == '.')
+                {
+                    b.ry += My[i];
+                    b.rx += Mx[i];
+                    if (arr[b.ry + My[i]][b.rx + Mx[i]] == '.')
+                    {
+                        b.ry += My[i];
+                        b.rx += Mx[i];
+                    }
+                }
+                if (arr[b.ry + My[i]][b.rx + Mx[i]] == 'O')
+                {
+                }
+            }
+        }
+    }
 }
 
 void solve()
@@ -21,8 +62,20 @@ void solve()
         {
             if (arr[i][j] == 'R')
             {
-                y = i;
-                x = j;
+                arr[i][j] == '.';
+                Ry = i;
+                Rx = j;
+            }
+            else if (arr[i][j] == 'B')
+            {
+                arr[i][j] == '.';
+                By = i;
+                Bx = j;
+            }
+            if (arr[i][j] == 'O')
+            {
+                Oy = i;
+                Ox = j;
             }
         }
     }
