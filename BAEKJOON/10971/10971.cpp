98f93b086cac6da10e31530fc9e2dbad93b1d30c
Kim._.jazz <jeong011010@naver.com>
2022-03-12 14:18:13 +0900

    add cpp file [10971.cpp]

---
+++ BAEKJOON/10971/10971.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+
+void bfs()
+{
+}
+
+void dijkstra()
+{
+}
+
+void solve()
+{
+    cin >> N;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
