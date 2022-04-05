3cf11a61aabebbd13d62957350261f53e395bbac
Kim._.jazz <jeong011010@naver.com>
2022-04-05 18:49:15 +0900

    update cpp file [10891.cpp]

--- BAEKJOON/10891/10891.cpp
+++ BAEKJOON/10891/10891.cpp
@@ -15,22 +15,22 @@ void cactus(int cur, int par)
     for (int i = 0; i < v[cur].size(); i++)
     {
         if (v[cur][i] == par || dfsn[v[cur][i]] >= dfsn[cur])
-            continue;
+            continue; // 다음 정점이 왔던곳이라면 스킵
         if (visit[v[cur][i]])
-        {
+        { // 다음 정점에 방문한 적 있으면 사이클 확인
             int tmp = cur;
             while (tmp != parent[v[cur][i]])
             {
-                if (visit[tmp] == 2)
+                if (visit[tmp] == 2) // 사이클 확인중 visit가 이미 2라면 이미 사이클 한번 돈 단순 사이클이므로 cactus가 아님.
                 {
                     cout << "Not cactus";
                     exit(0);
                 }
-                visit[tmp]++;
+                visit[tmp]++; // 방문 수를 늘려 visit가 2라면 사이클 한번 돈것.
                 tmp = parent[tmp];
             }
         }
-        else
+        else // 다음 정점에 방문한 적 없다면
             cactus(v[cur][i], cur);
     }
 }
