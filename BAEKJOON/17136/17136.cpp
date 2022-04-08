e631700ecdb2bf5acd1dbb2150c21ec0f66514d5
Kim._.jazz <jeong011010@naver.com>
2022-04-08 18:05:21 +0900

    add cpp file [17136.cpp]

---
+++ BAEKJOON/17136/17136.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int paper[5];
+bool arr[10][10];
+
+void f(int n)
+{
+}
+
+void solve()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+            cin >> arr[i][j];
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
