891b96166b6242b1973f6da72f82f3157a612926
Kim._.jazz <jeong011010@naver.com>
2022-04-28 04:03:13 +0900

    add cpp file [1208.cpp]

---
+++ BAEKJOON/1208/1208.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int N, S;
+int arr[40];
+
+void solve()
+{
+    cin >> N >> S;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    long long sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        while ()
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
