b4b9c901be1e984a0c8d0392ebea6e5d70c9925f
Kim._.jazz <jeong011010@naver.com>
2022-04-28 13:45:04 +0900

    update cpp file [2239.cpp]

---
+++ BAEKJOON/2239/2239.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool check = false;
+vector<int> v[9];
+int block[9][9][2] = {
+    {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}},
+    {{0, 3}, {0, 4}, {0, 5}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}},
+    {{0, 6}, {0, 7}, {0, 8}, {1, 6}, {1, 7}, {1, 8}, {2, 6}, {2, 7}, {2, 8}},
+    {{3, 0}, {3, 1}, {3, 2}, {4, 0}, {4, 1}, {4, 2}, {5, 0}, {5, 1}, {5, 2}},
+    {{3, 3}, {3, 4}, {3, 5}, {4, 3}, {4, 4}, {4, 5}, {5, 3}, {5, 4}, {5, 5}},
+    {{3, 6}, {3, 7}, {3, 8}, {4, 6}, {4, 7}, {4, 8}, {5, 6}, {5, 7}, {5, 8}},
+    {{6, 0}, {6, 1}, {6, 2}, {7, 0}, {7, 1}, {7, 2}, {8, 0}, {8, 1}, {8, 2}},
+    {{6, 3}, {6, 4}, {6, 5}, {7, 3}, {7, 4}, {7, 5}, {8, 3}, {8, 4}, {8, 5}},
+    {{6, 6}, {6, 7}, {6, 8}, {7, 6}, {7, 7}, {7, 8}, {8, 6}, {8, 7}, {8, 8}},
+};
+
+int f(vector<int> board[9], int y, int x)
+{
+    bool check[10];
+    for (int i = 0; i < 10; i++)
+        check[i] = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+        check[board[y][i]] = 1;
+        check[board[i][x]] = 1;
+    }
+    int n;
+    bool b = false;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (block[i][j][0] == y && block[i][j][1] == x)
+            {
+                n = i;
+                b = true;
+                break;
+            }
+        }
+        if (b)
+            break;
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        check[board[block[n][i][0]][block[n][i][1]]] = 1;
+    }
+    int cnt = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        if (!check[i])
+        {
+            cnt++;
+            n = i;
+        }
+    }
+    if (cnt == 1)
+        return n;
+    return 0;
+}
+
+void sudoku(vector<int> board[9])
+{
+
+    if (check)
+        return;
+
+    bool full = true;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (board[i][j] == 0)
+            {
+                full = false;
+                int tmp = f(board, i, j);
+                if (tmp != 0)
+                {
+                    board[i][j] = tmp;
+                    sudoku(board);
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if (full)
+    {
+        check = true;
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cout << board[i][j];
+            }
+            cout << '\n';
+        }
+    }
+}
+
+void solve()
+{
+    string tmp;
+    vector<int> b[9];
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> tmp;
+        for (int j = 0; j < 9; j++)
+        {
+            b[i].push_back(tmp[j] - 48);
+        }
+    }
+    sudoku(b);
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
