5e1430581663d60dc5f6ba26e23d92c0ea137b54
Kim._.jazz <jeong011010@naver.com>
2022-06-01 17:07:33 +0900

    update cpp file [14890.cpp]

--- BAEKJOON/14890/14890.cpp
+++ BAEKJOON/14890/14890.cpp
@@ -19,41 +19,79 @@ void input()
 
 void checking()
 {
-    int ans = N;
-    for (int i = 0; i < N; i++)
+    int ans = N * 2;
+    for (int k = 0; k < 2; k++)
     {
-        int n = board[i][0];
-        int up = 1, down = 0;
-        for (int j = 1; j < N; j++)
+        for (int i = 0; i < N; i++)
         {
-            if (board[i][j] == n)
+            int n = k == 0 ? board[i][0] : board[0][i];
+            int up = 1, down = 0;
+            for (int j = 1; j < N; j++)
             {
-                up++;
-            }
-            else if (board[i][j] == n - 1)
-            {
-                n--;
-                down++;
-            }
-            else if (board[i][j] == n + 1)
-            {
-                if (up == L)
+                int tile = k == 0 ? board[i][j] : board[j][i];
+                if (tile == n)
                 {
-                    up = 1;
+                    if (down > 0)
+                    {
+                        down--;
+                    }
+                    else
+                        up++;
+                }
+                else if (tile == n - 1)
+                {
+                    down--;
+                    if (down > 0)
+                    {
+                        ans--;
+                        down = 0;
+                        // cout << k << ' ' << i << ',' << j << ":1\n";
+                        break;
+                    }
+                    n--;
+                    down = L;
+                }
+                else if (tile == n + 1)
+                {
+                    if (down != 0)
+                    {
+                        ans--;
+                        down = 0;
+                        // cout << k << ' ' << i << ',' << j << ":2\n";
+                        break;
+                    }
+                    if (up >= L)
+                    {
+                        n++;
+                        up = 1;
+                    }
+                    else
+                    {
+                        ans--;
+                        down = 0;
+                        // cout << k << ' ' << i << ',' << j << ":3\n";
+                        break;
+                    }
                 }
                 else
                 {
                     ans--;
+                    down = 0;
                     break;
                 }
             }
+            // cout << down << '\n';
+            if (down > 1)
+                ans--;
         }
     }
+    cout << ans;
 }
 
 void solve()
 {
     input();
+    checking();
 }
 
 int main()
