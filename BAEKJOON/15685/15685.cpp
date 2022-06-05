4da7ed701054445d24a03133ed6baf7e5f6c391f
Kim._.jazz <jeong011010@naver.com>
2022-06-06 02:53:42 +0900

    add cpp file [15685.cpp]

---
+++ BAEKJOON/15685/15685.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+bool board[101][101];
+vector<pair<int, int>> gen[10];
+
+void input()
+{
+    int x, y, d, g;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x >> y >> d >> g;
+        board[]
+    }
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
