836d170652e58ed0bbad0750b2735717995b3ede
Kim._.jazz <jeong011010@naver.com>
2022-06-05 19:12:49 +0900

    update cpp file [15684.cpp]

--- BAEKJOON/15684/15684.cpp
+++ BAEKJOON/15684/15684.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
+map<string, bool> m;
 int N, M, H;
 
-int lad[10][30];
+int lad[30][10];
 
 void init()
 {
@@ -24,14 +26,45 @@ void input()
     for (int i = 0; i < M; i++)
     {
         cin >> a >> b;
-        lad[b - 1][a - 1] = a;
-        lad[b - 1][a] = a - 1;
+        lad[a - 1][b - 1] = b;
+        lad[a - 1][b] = b - 1;
     }
 }
 
 int n = 0, ans = 4;
 
-void run()
+bool key()
+{
+    string s = "";
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            s += to_string(lad[i][j]);
+        }
+    }
+    if (m.find(s) != m.end())
+    {
+        return false;
+    }
+    m.insert({s, 0});
+    return true;
+}
+
+void print()
+{
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << lad[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
+
+bool run()
 {
     for (int i = 0; i < N; i++)
     {
@@ -41,21 +74,54 @@ void run()
             line = lad[j][line];
         }
         if (line != i)
-            return;
+            return false;
     }
     ans = min(ans, n);
+    // print();
+    // cout << n << '\n';
+    return true;
 }
 
-void setting()
+void setting(int y, int x)
 {
-    for (int i = 0; i < H; i++)
+    if (!key())
+        return;
+    if (run())
+        return;
+    if (n > 3)
+        return;
+    // print();
+    for (int i = y; i < H; i++)
     {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if (i == y && j <= x)
+                continue;
+            if (lad[i][j] == j && lad[i][j + 1] == j + 1)
+            {
+                lad[i][j] = j + 1;
+                lad[i][j + 1] = j;
+                n++;
+                // cout << i << ',' << j << 'i' << '\n';
+                setting(i, j);
+                // cout << i << ',' << j << 'd' << '\n';
+                lad[i][j] = j;
+                lad[i][j + 1] = j + 1;
+                n--;
+            }
+        }
     }
 }
 
 void solve()
 {
     input();
+    // print();
+    setting(0, -1);
+    if (ans > 3)
+        cout << -1;
+    else
+        cout << ans;
 }
 
 int main()
