445012533a44d27fa3648d0b1203a2d70547083e
Kim._.jazz <jeong011010@naver.com>
2022-06-09 23:48:19 +0900

    add cpp file [17143.cpp]

---
+++ BAEKJOON/17143/17143.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int R, C, M;
+int board[100][100];
+
+void input()
+{
+    cin >> R >> C >> M;
+    int r, c, s, d, z;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> r >> c >> s >> d >> z;
+    }
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
